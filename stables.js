18ac24495652b4ba8d8da052f42ebf17cb074905
Belinda To <bto10@my.bcit.ca>
2024-09-26 14:37:49 -0700

    add operators to my stable

---
+++ stables.js
@@ -0,0 +1,9 @@
+console.log("horses are great!")
+let horseName = "Strawberry"; // This is a comment or a note
+horseName = "donut";
+let horseAge = 100;
+console.log(horseName);
+console.log(horseAge);
+let isHorseInStable = false;
+console.log(isHorseInStable);
+console.log(typeof 21)
